b879deb3e4fa841312e00417bbb36e3f55b8f9cd
Raj Datta <rajdat2002@gmail.com>
2025-08-05 20:46:01 -0400

    fix stack overflow for maze dimension above 300

--- maze.js
+++ maze.js
@@ -1,30 +1,4 @@
 function generateSquareMaze(dimension) {
-
-    function iterate(field, x, y) {
-        field[x][y] = false;
-        while(true) {
-            directions = [];
-            if(x > 1 && field[x-2][y] == true) {
-                directions.push([-1, 0]);
-            }
-            if(x < field.dimension - 2 && field[x+2][y] == true) {
-                directions.push([1, 0]);
-            }
-            if(y > 1 && field[x][y-2] == true) {
-                directions.push([0, -1]);
-            }
-            if(y < field.dimension - 2 && field[x][y+2] == true) {
-                directions.push([0, 1]);
-            }
-            if(directions.length == 0) {
-                return field;
-            }
-            dir = directions[Math.floor(Math.random()*directions.length)];
-            field[x+dir[0]][y+dir[1]] = false;
-            field = iterate(field, x+dir[0]*2, y+dir[1]*2);
-        }
-    }
-
     // Initialize the field.
     var field = new Array(dimension);
     field.dimension = dimension;
@@ -35,11 +9,46 @@ function generateSquareMaze(dimension) {
         }
     }
 
-    // Gnerate the maze recursively.
-    field = iterate(field, 1, 1);
+    var stack = [];
+    var startX = 1, startY = 1;
+    
+    field[startX][startY] = false;
+    stack.push([startX, startY]);
+    
+    while (stack.length > 0) {
+        var current = stack[stack.length - 1];
+        var x = current[0];
+        var y = current[1];
+        
+        var directions = [];
+        if(x > 1 && field[x-2][y] == true) {
+            directions.push([-1, 0]);
+        }
+        if(x < field.dimension - 2 && field[x+2][y] == true) {
+            directions.push([1, 0]);
+        }
+        if(y > 1 && field[x][y-2] == true) {
+            directions.push([0, -1]);
+        }
+        if(y < field.dimension - 2 && field[x][y+2] == true) {
+            directions.push([0, 1]);
+        }
+        
+        if(directions.length == 0) {
+            stack.pop();
+        } else {
+            // Choose a random direction and move
+            var dir = directions[Math.floor(Math.random() * directions.length)];
+            var newX = x + dir[0] * 2;
+            var newY = y + dir[1] * 2;
+            
+            // Mark the wall and the new cell as path
+            field[x + dir[0]][y + dir[1]] = false;
+            field[newX][newY] = false;
+            
+            stack.push([newX, newY]);
+        }
+    }
     
     return field;
-
 }
-
-
